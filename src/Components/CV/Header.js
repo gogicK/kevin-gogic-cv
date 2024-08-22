e61eeffd71d646696cb5b6f19f413bd24aed9507
gogick <kevin.gogic@gmail.com>
2024-08-22 15:48:38 +0200

    fix verbose

--- src/Components/CV/Header.js
+++ src/Components/CV/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
                 </div>
                 <div className="w-full flex-1 flex flex-col justify-center gap-4 md:mt-0 sm:mt-8 xs:mt-4">
                     <h1 className="text-3xl font-bold text-slate-900">Bonjour, je suis Kévin Gogic</h1>
-                    <p className="text-slate-500">Développeur web passionné, spécialisée dans la création de sites dynamiques et réactifs. Polyvalente en front-end et back-end, je conçois des solutions optimisées et centrées sur l'utilisateur, tout en suivant les dernières tendances du web.</p>
+                    <p className="text-slate-500"><b>Développeur web passionné</b>, spécialisée dans la création de sites dynamiques et réactifs. Polyvalente en front-end et back-end, je conçois des solutions optimisées et centrées sur l'utilisateur, tout en suivant les dernières tendances du web.</p>
                 </div>
             </div>
         </>
