264412282e474a7f62224425e096a5c0cee76301
gogick <kevin.gogic@gmail.com>
2024-09-09 19:58:53 +0200

    fix navbar close correctly

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,40 +2,49 @@ import { Link } from "react-router-dom"
 import Icon from "../Icon"
 import DownloadButton from "./DownloadButton"
 
-const Navbar = () => {
+
+export default function Navbar() {
+      function handleClick() {
+            const menu = document.getElementById('menu-toggle');
+            if (menu) {
+                  menu.checked = menu.checked ? false : true;
+            }
+      }
+
       return (
-            <nav className="font-poppins font-bold flex items-center lg:justify-end space-x-4 justify-center min-h-[76px] p-4 sticky top-0 z-10 bg-white shadow-xl">
-                  <input className="peer hidden" type="checkbox" id="menu-toggle" />
-                  <div className="absolute left-0 top-4"><Link to={'/kevin-webdev/'} >{Icon.logo}</Link></div>
-                  <ul className="ps-0 hidden text-center lg:!flex lg:!items-center lg:!justify-center lg:space-x-4 peer-checked:ml-0 peer-checked:block  peer-checked:space-y-6 lg:!space-y-0" id="menu">
-                        {[
-                              ['Accueil', '/kevin-webdev/'],
-                              ['CV classique', '/kevin-webdev/cvA4'],
-                              ['À propos', '/kevin-webdev/about'],
-                        ].map(([title, url], index) => (
-                              <li key={index}>
-                                    <Link to={url} className="rounded-lg px-3 py-2 text-slate-700 hover:bg-indigo-500 hover:text-white">
-                                          {title}
+            <>
+                  <nav className="font-poppins font-bold flex items-center lg:justify-end space-x-4 justify-center min-h-[76px] p-4 sticky top-0 z-10 bg-white shadow-xl">
+                        <input className="peer hidden" type="checkbox" id="menu-toggle" />
+                        <div className="absolute left-0 top-4"><Link onClick={handleClick} to={'/kevin-webdev/'} >{Icon.logo}</Link></div>
+                        <ul className="ps-0 hidden text-center lg:!flex lg:!items-center lg:!justify-center lg:!space-x-4 peer-checked:ml-0 peer-checked:block  peer-checked:space-y-6 lg:!space-y-0" id="menu">
+                              {[
+                                    ['Accueil', '/kevin-webdev/'],
+                                    ['CV classique', '/kevin-webdev/cvA4'],
+                                    ['À propos', '/kevin-webdev/about'],
+                              ].map(([title, url], index) => (
+                                    <li key={index}>
+                                          <Link onClick={handleClick} to={url} className="rounded-lg px-3 py-2 text-slate-700 hover:bg-indigo-500 hover:text-white">
+                                                {title}
+                                          </Link>
+                                    </li>
+                              ))}
+                              <li>
+                                    <Link onClick={handleClick} target="_blank" to={'https://github.com/gogicK/kevin-webdev'} className="rounded-lg px-3 py-2 text-indigo-500 border-indigo-500 border-2 hover:bg-indigo-500 hover:text-white">
+                                          Regardez le code
                                     </Link>
                               </li>
-                        ))}
-                        <li>
-                              <Link target="_blank" to={'https://github.com/gogicK/kevin-webdev'} className="rounded-lg px-3 py-2 text-indigo-500 border-indigo-500 border-2 hover:bg-indigo-500 hover:text-white">
-                                    Regardez le code
-                              </Link>
-                        </li>
-                        <li>
-                              <DownloadButton url={process.env.PUBLIC_URL + '/gogic-kevin-cv-dev.pdf'} label="Téléchargez mon CV" />
-                        </li>
-                        <li>
-                              <Link to={'/kevin-webdev/contact'} className="rounded-lg px-3 py-2 text-indigo-500 border-indigo-500 border-2 hover:bg-indigo-500 hover:text-white">
-                                    Contactez-moi
-                              </Link>
-                        </li>
-                  </ul>
-                  <label htmlFor="menu-toggle" className="cursor-pointer lg:hidden block absolute top-4 right-4">
-                        <svg
-                              className="animate-[wiggle_2s_linear_infinite] fill-current text-gray-900"
+                              <li>
+                                    <DownloadButton onClick={handleClick} url={process.env.PUBLIC_URL + '/gogic-kevin-cv-dev.pdf'} label="Téléchargez mon CV" />
+                              </li>
+                              <li>
+                                    <Link onClick={handleClick} to={'/kevin-webdev/contact'} className="rounded-lg px-3 py-2 text-indigo-500 border-indigo-500 border-2 hover:bg-indigo-500 hover:text-white">
+                                          Contactez-moi
+                                    </Link>
+                              </li>
+
+                        </ul>
+                        <svg onClick={handleClick}
+                              className="animate-[wiggle_2s_linear_infinite] fill-current text-gray-900 cursor-pointer lg:hidden block absolute top-4 right-4"
                               xmlns="http://www.w3.org/2000/svg"
                               width="40"
                               height="40"
@@ -44,9 +53,7 @@ const Navbar = () => {
                               <title>menu</title>
                               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
                         </svg>
-                  </label>
-            </nav>
+                  </nav >
+            </>
       )
 }
-
-export default Navbar;
