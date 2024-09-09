98938584f816dc9d54795b8e628d0e5f14f42215
gogick <kevin.gogic@gmail.com>
2024-09-09 15:58:28 +0200

    modify text description section & about

--- src/Components/HomePage/Description.js
+++ src/Components/HomePage/Description.js
@@ -8,7 +8,7 @@ export default function Description() {
                     Mon parcours m'a permis de <b>maîtriser</b> un large éventail <span className="text-secondaryDark">de technologies et de langages de programmation.</span><br />
                     Au fil des années, j'ai eu l'opportunité de travailler sur des <b>projets variés</b>, allant du <span className="text-secondaryDark">développement de programmes</span> à travers des machines linux & windows, <span className="text-secondaryDark">à la création de site web.</span><br />
                     Chaques projets m'a permis de <b>renforcer</b> mes compétences <span className="text-secondaryDark">en architecture logicielle, en résolution de problèmes, et en optimisation de code.</span><br />
-                    Je suis particulièrement <b>attentif</b> <span className="text-secondaryDark">à la qualité du code, à la maintenabilité des solutions</span> que je propose, et à la <span className="text-secondaryDark">satisfaction des utilisateurs</span> finaux.
+                    <b>Attentif</b> <span className="text-secondaryDark">à la qualité du code, à la maintenabilité des solutions</span> et à la <span className="text-secondaryDark">satisfaction des utilisateurs</span> finaux.
                 </p>
             </div>
             <div className="col-span-3 lg:col-span-1 flex justify-center items-center mb-4 lg:mb-0"><img className="max-w-96 max-h-96 border-black border-2 rounded-full m-4" alt="capybara developpeur" src={process.env.PUBLIC_URL + '/images/capybara-developpeur.png'} /></div>
@@ -17,8 +17,8 @@ export default function Description() {
             <div className="order:4 lg:order-5 col-span-3 lg:col-span-2 p-4 lg:p-20 flex flex-col justify-center gap-4">
                 <h2>Designer</h2>
                 <p className="italic indent-12 text-justify text-base lg:text-lg">
-                    Je suis un <span className="text-secondaryDark">designer en herbe</span>, passionné par la <b>création</b> et animé par une <span className="text-secondaryDark">forte volonté d'apprendre.</span><br />
-                    Chaque projet est pour moi l'occasion d'<b>explorer</b> <span className="text-secondaryDark">de nouvelles idées, d'acquérir de nouvelles compétences et de donner vie à des concepts uniques.</span>
+                    <span className="text-secondaryDark">Designer en herbe</span>, passionné par la <b>création</b> et animé par une <span className="text-secondaryDark">forte volonté d'apprendre.</span><br />
+                    Chaque nouveau projet est pour moi l'occasion d'<b>explorer</b> <span className="text-secondaryDark">de nouvelles idées, d'acquérir de nouvelles compétences et de donner vie à des concepts uniques.</span>
                 </p>
             </div>
         </section>
--- src/routes/About.js
+++ src/routes/About.js
@@ -3,14 +3,14 @@ export default function About() {
         <>
             <div className="bg-pattern">
                 <section className='font-poppins flex flex-col justify-center items-center px-10 lg:px-40 min-h-[calc(100vh-76px)] space-y-4 leading-relaxed'>
-                    <div className="bg-white p-8 shadow-xl rounded-xl">
+                    <div className="bg-white p-8 shadow-xl rounded-xl my-4">
                         <div>
-                            <h1 className="p-4">A propos de ce site</h1>
+                            <h1 className="p-4 text-2xl lg:text-4xl">A propos de ce site</h1>
                             <p className="indent-8 text-justify">Bienvenue sur mon site, une vitrine moderne et performante conçue pour offrir une expérience utilisateur fluide et rapide. Ce site est construit avec React, un framework JavaScript puissant qui permet de créer des interfaces dynamiques et réactives. Pour le style, j'ai opté pour Tailwind CSS, un framework qui me permet de personnaliser chaque élément avec précision tout en maintenant un code propre et maintenable. Hébergé sur une infrastructure serverless, le site est entièrement statique, garantissant une rapidité de chargement optimale et une grande fiabilité. Explorez-le pour découvrir une technologie de pointe alliée à un design élégant.</p>
                         </div>
                         <div>
-                            <h1 className="p-4">A propos de moi</h1>
-                            <p className="indent-8 text-justify">Je m'appelle Kévin Gogic, développeur web avec une passion pour la création de solutions numériques innovantes et efficaces. Avec plusieurs années d'expérience dans le développement de sites web et d'applications, je maîtrise un large éventail de technologies et de langages, ce qui me permet de m'adapter à divers projets et défis. Mon approche du développement est centrée sur l'utilisateur, alliant performance, esthétique et facilité d'utilisation. Je suis toujours à la recherche de nouvelles opportunités pour apprendre, grandir et appliquer mes compétences à des projets qui font la différence. Si vous recherchez un développeur capable de transformer vos idées en réalité, je suis à votre disposition.</p>
+                            <h1 className="p-4 text-2xl lg:text-4xl">A propos de moi</h1>
+                            <p className="indent-8 text-justify">Kévin Gogic, développeur web avec une passion pour la création de solutions numériques innovantes et efficaces. Avec plusieurs années d'expérience dans le développement de sites web et d'applications, je maîtrise un large éventail de technologies et de langages, ce qui me permet de m'adapter à divers projets et défis. Mon approche du développement est centrée sur l'utilisateur, alliant performance, esthétique et facilité d'utilisation. Je suis toujours à la recherche de nouvelles opportunités pour apprendre, grandir et appliquer mes compétences à des projets qui font la différence. Si vous recherchez un développeur capable de transformer vos idées en réalité, je suis à votre disposition.</p>
                         </div>
                     </div>
                 </section >
