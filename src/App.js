db14e9232b074a02e2893e85df3aa4647714d11f
gogick <kevin.gogic@gmail.com>
2024-09-22 14:10:33 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import CVA4 from "./routes/CVA4";
 import About from "./routes/About";
 import HomePage from "./routes/HomePage";
@@ -10,15 +10,13 @@ import Navbar from './Components/Navbar';
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route index path="/kevin-webdev" element={<HomePage />} />
-          <Route path="/kevin-webdev/cvA4" element={<CVA4 />} />
-          <Route path="/kevin-webdev/about" element={<About />} />
-          <Route path="/kevin-webdev/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index path="/kevin-webdev" element={<HomePage />} />
+        <Route path="/kevin-webdev/cvA4" element={<CVA4 />} />
+        <Route path="/kevin-webdev/about" element={<About />} />
+        <Route path="/kevin-webdev/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
